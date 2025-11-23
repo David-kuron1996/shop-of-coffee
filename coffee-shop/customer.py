4c4174e45b0b2eeb7b4833f7b7940423c1795cc1
David <davidkuron1996@gmail.com>
2025-11-23 16:46:35 +0300

    add this file

--- coffee-shop/customer.py
+++ coffee-shop/customer.py
@@ -14,3 +14,31 @@ class Customer:
         if len(value) < 1 or len(value) > 15:
             raise ValueError("Name must be between 1 and 15 characters")
         self._name = value
+        def orders(self):
+        return self._orders
+
+    def coffees(self):
+        unique_coffees = []
+        for order in self._orders:
+            if order.coffee not in unique_coffees:
+                unique_coffees.append(order.coffee)
+        return unique_coffees
+
+    def create_order(self, coffee, price):
+        from order import Order
+        order = Order(self, coffee, price)
+        self._orders.append(order)
+        return order
+
+    @classmethod
+    def most_aficionado(cls, coffee):
+        if not coffee.orders():
+            return None
+        
+        customer_spending = {}
+        for order in coffee.orders():
+            customer = order.customer
+            if customer not in customer_spending:
+                customer_spending[customer] = 0
+            customer_spending[customer] += order.price
+        return max(customer_spending, key=customer_spending.get)
