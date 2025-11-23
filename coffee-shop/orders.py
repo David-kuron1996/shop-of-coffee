f16ab2b9505e7ea0dcba2173acab3ba80cdbd2f2
David <davidkuron1996@gmail.com>
2025-11-23 17:01:49 +0300

    add this file

---
+++ coffee-shop/orders.py
@@ -0,0 +1,9 @@
+class Order:
+    def __init__(self, customer, coffee, price):
+        self._customer = None
+        self._coffee = None
+        self._price = None
+        
+        self.customer = customer
+        self.coffee = coffee
+        self.price = price
