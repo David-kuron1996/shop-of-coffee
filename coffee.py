d950273669a5f43928b306f241d08454519afdde
David <davidkuron1996@gmail.com>
2025-12-02 11:26:12 +0300

    add this file

---
+++ coffee.py
@@ -0,0 +1,34 @@
+class Coffee:
+    def __init__(self, name):
+        self._name = None
+        self.name = name
+        self._orders = []
+
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Name must be a string")
+        if len(value) < 3:
+            raise ValueError("Name must be at least 3 characters long")
+        self._name = value
+
+    def orders(self):
+        return self._orders
+
+    def customers(self):
+        unique_customers = []
+        for order in self._orders:
+            if order.customer not in unique_customers:
+                unique_customers.append(order.customer)
+        return unique_customers
+
+    def num_orders(self):
+        return len(self._orders)
+def average_price(self):
+        if not self._orders:
+            return 0
+        total = sum(order.price for order in self._orders)
+        return total / len(self._orders)
---
+++ customer.py
@@ -0,0 +1,44 @@
+class Customer:
+    def __init__(self, name):
+        self._name = None
+        self.name = name
+        self._orders = []
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Name must be a string")
+        if len(value) < 1 or len(value) > 15:
+            raise ValueError("Name must be between 1 and 15 characters")
+        self._name = value
+        def orders(self):
+          return self._orders
+
+    def coffees(self):
+        unique_coffees = []
+        for order in self._orders:
+            if order.coffee not in unique_coffees:
+                unique_coffees.append(order.coffee)
+        return unique_coffees
+
+    def create_order(self, coffee, price):
+        from orders import Order
+        order = Order(self, coffee, price)
+        self._orders.append(order)
+        return order
+
+    @classmethod
+    def most_aficionado(cls, coffee):
+        if not coffee.orders():
+            return None
+        
+        customer_spending = {}
+        for order in coffee.orders():
+            customer = order.customer
+            if customer not in customer_spending:
+                customer_spending[customer] = 0
+            customer_spending[customer] += order.price
+        return max(customer_spending, key=customer_spending.get)
